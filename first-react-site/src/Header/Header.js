39dcbf4f2e847ef2d7591cda068aedb5bc0af614
Rizvan <xl92@mail.ru>
2022-10-07 20:24:51 +0300

    finish header section

--- first-react-site/src/Header/Header.js
+++ first-react-site/src/Header/Header.js
@@ -1,8 +1,20 @@
+import './header.css';
+import slide from '../Header/slide-1.jpg';
 function Header() {
     return(
-        <>
-        <h1>Title</h1>
-        </>
+        <header className="header">
+            <div className="container">
+                <div className="header__box">
+                    <div className="header__top">
+                        <h1 className="header__title">САНТЕХНИК ГРОЗНЫЙ</h1>
+                        <a className="btn" href="tel:11122233 " ><span>111222333</span></a>
+                    </div>
+                    <div className="header__bottom">
+                        <img className="header__img" src={slide} alt=""  />
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 export default Header;
